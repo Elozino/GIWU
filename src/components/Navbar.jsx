1c983cd38582fc610e2cb5fbfa675ba61c9fcb5f
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-07-16 03:17:12 +0100

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Icon from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "../index.css"
 
 function Navbar() {
+  const [menu, setMenu] = useState(false)
   return (
     <>
       <nav className="flex justify-between w-full px-5 py-4 bg-black text-white">
@@ -10,8 +12,7 @@ function Navbar() {
           <div className="px-3 font-bold text-2xl">GIWU</div>
           <div
             className="md:block w-full border-solid border-l-2 ">
-            <ul className="flex border-white px-3 "
-              style={{ position: "absolute", top: "70px", width: "100%", left: "0px", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexDirection: "column", height: "calc(100vh - 70px)", backgroundColor: "rgba(0, 0, 0, 0.7)", }}
+            <ul className={menu ? "link" : "hidden lg:flex border-white px-3"}
             >
               <li className="cursor-pointer mr-10">
                 <Link to="/">HOME</Link>
@@ -31,7 +32,7 @@ function Navbar() {
         <button className="text-black bg-pink-200 h-full p-2 rounded hidden md:block">
           CONTACT US
         </button>
-        <button className="md:hidden cursor-pointer" onClick={""}>
+        <button className="md:hidden cursor-pointer" onClick={() => setMenu(prev => !prev)}>
           <Icon.FaBars size={24} />
         </button>
       </nav>
